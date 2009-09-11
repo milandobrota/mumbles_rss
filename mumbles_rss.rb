86ce1595c73cbc379a4b55472292899ceb44cfbe
Milan Dobrota <elitecoding@gmail.com>
2009-09-11 14:50:51 +0200

    script now wont break if 300s or 400s are returned

--- mumbles_rss.rb
+++ mumbles_rss.rb
@@ -13,13 +13,13 @@ end
 loop do
   sleep(30)
   feeds.each do |feed|
-    unless feed == 0 || feed.new_entries.size == 0
+    unless feed.is_a? Fixnum || feed.new_entries.size == 0
       feed.new_entries.each do |entry|
         system "mumbles-send \"#{entry.title}\" \"#{Sanitize.clean(entry.summary)}\""
       end
       feed.new_entries = []
     end
     temp_feed = Feedzirra::Feed.update(feed)
-    feed = temp_feed unless temp_feed == 0
+    feed = temp_feed unless temp_feed.is_a? Fixnum
   end
 end
